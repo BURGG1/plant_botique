314b1caaea1690de4fdfa59bbfface3c58031a2b
BURGG1 <torresadrian00007@gmail.com>
2026-04-18 13:36:36 +0800

    intial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="font-bold text-4xl text-white">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App
